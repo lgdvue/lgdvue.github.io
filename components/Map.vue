24b957425c2138e658b67e05533e56c9e22c3374
Miguel Ángel <migueabellan@gmail.com>
2019-05-25 20:24:56 +0200

    documentación está

--- components/Map.vue
+++ components/Map.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="map-wrap" style="height: 80vh; position: relative; z-index: 1">
-    <no-ssr>
-      <l-map :zoom="6" :center="[40.4183083, -3.70275]">
-        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
-        <Market
-          v-for="location in locations"
-          :key="location.id"
-          :location="location"
-        />
-      </l-map>
-    </no-ssr>
+    <l-map :zoom="6" :center="[40.4183083, -3.70275]">
+      <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+      <Market
+        v-for="location in locations"
+        :key="location.id"
+        :location="location"
+      />
+    </l-map>
   </div>
 </template>
 
